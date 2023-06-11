ef61748d17ccc1637bd7d6516763d734b3c66dc9
Tolga Özgün <tolgaozgunn@gmail.com>
2023-06-11 18:32:55 +0300

    Setup frontend and backend

---
+++ backend/src/main/java/com/tolgaozgun/gdscturkweb/GdscturkwebApplication.java
@@ -0,0 +1,13 @@
+package com.tolgaozgun.gdscturkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscturkwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscturkwebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tolgaozgun/gdscturkweb/GdscturkwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgaozgun.gdscturkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscturkwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
